2129e3e029d5e725735705eb6ae7d92490ca3e0c
cheesemitch <edin.cizmic@agnoscis.io>
2024-01-09 00:45:24 +0100

    Updates

---
+++ assets/myObjectives.fa355c4d.js
@@ -0,0 +1,1 @@
+import{_ as r,a as t,l as c,e as n}from"./index.8f5ee334.js";const a={};function s(e,_){const o=n("router-view");return t(),c(o,{key:e.$route.path})}var f=r(a,[["render",s]]);export{f as default};
---
+++ assets/teamOKR.33575a9f.js
@@ -0,0 +1,1 @@
+import{_ as r,a as t,l as a,e as n}from"./index.8f5ee334.js";const c={};function s(e,_){const o=n("router-view");return t(),a(o,{key:e.$route.path})}var f=r(c,[["render",s]]);export{f as default};
